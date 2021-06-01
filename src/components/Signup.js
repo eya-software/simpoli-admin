3800d9beafe296c156d1601da9a3312c1a50f4c8
Nathan Gong <nathan.gong22@bcp.org>
2021-05-31 21:36:28 -0700

    Fix link issue

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -60,7 +60,7 @@ export default function Signup() {
             </div>
 
             <div className="text-sm">
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+              <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
                 Forgot your password?
               </a>
             </div>
