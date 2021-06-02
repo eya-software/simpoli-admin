64f32299f29363e9a2099622361d30b89c88a2a8
Nathan Gong <nathan.gong22@bcp.org>
2021-06-02 12:45:20 -0700

    Fix href issue

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -41,7 +41,7 @@ export default function Dashboard() {
                         itemIdx === 0 ? (
                           <Fragment key={item}>
                             <a
-                              href="#"
+                              href="/"
                               className="h-full inline-flex items-center px-3 py-2 border-b-2 border-transparent text-base font-medium leading-6 text-black border-indigo-600 dark:text-dark-high-emphasis transition"
                             >
                               {item}
@@ -50,7 +50,7 @@ export default function Dashboard() {
                         ) : (
                           <a
                             key={item}
-                            href="#"
+                            href="/"
                             className="h-full inline-flex items-center px-3 py-2 border-b-2 border-transparent text-base font-medium leading-6 text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:text-dark-high-emphasis focus:outline-none focus:text-gray-700 focus:border-gray-300 transition"
                           >
                             {item}
@@ -93,7 +93,7 @@ export default function Dashboard() {
                               <Menu.Item key="Your Profile">
                                 {({ active }) => (
                                   <a
-                                    href="#"
+                                    href="/"
                                     className={classNames(
                                       active ? "bg-gray-100" : "",
                                       "block px-4 py-2 text-sm text-gray-700"
@@ -144,7 +144,7 @@ export default function Dashboard() {
                     <Fragment key={item}>
                       {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                       <a
-                        href="#"
+                        href="/"
                         className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
                       >
                         {item}
@@ -153,7 +153,7 @@ export default function Dashboard() {
                   ) : (
                     <a
                       key={item}
-                      href="#"
+                      href="/"
                       className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                     >
                       {item}
@@ -183,7 +183,7 @@ export default function Dashboard() {
                   {profile.map((item) => (
                     <a
                       key={item}
-                      href="#"
+                      href="/"
                       className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
                     >
                       {item}
