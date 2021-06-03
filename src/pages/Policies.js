d598699ee1c2525920b5c11a353f989f75658a39
josephp1005 <40573986+josephp1005@users.noreply.github.com>
2021-06-03 15:36:04 -0700

    Update Policies.js

--- src/pages/Policies.js
+++ src/pages/Policies.js
@@ -3,14 +3,14 @@ import Page from "./Page";
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
 import Card from "@material-ui/core/Card";
 
-const people = [
+const policy = [
   {
     name: 'George Floyd Justice in Policing Act of 2020',
     description: 'Addresses issues regarding policing practices',
     image:
       'https://images.assettype.com/freepressjournal%2F2020-06%2F6a02b775-b91b-4c24-99c8-55976cb20966%2F27georgefloyd_superJumbo.jpg?rect=142%2C0%2C497%2C497&w=128&dpr=2.6',
   },
-  // More people...
+  // More policies...
 ]
 export default function Policies() {
   return (
@@ -19,13 +19,13 @@ export default function Policies() {
 	    <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
 	      <div className="flex-1 flex justify-between sm:hidden">
 	        <a
-	          href="#"
+	          href="/"
 	          className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
 	        >
 	          Previous
 	        </a>
 	        <a
-	          href="#"
+	          href="/"
 	          className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
 	        >
 	          Next
@@ -41,7 +41,7 @@ export default function Policies() {
 	        <div>
 	          <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
 	            <a
-	              href="#"
+	              href="/"
 	              className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
 	            >
 	              <span className="sr-only">Previous</span>
@@ -49,20 +49,20 @@ export default function Policies() {
 	            </a>
 	            {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
 	            <a
-	              href="#"
+	              href="/"
 	              aria-current="page"
 	              className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
 	            >
 	              1
 	            </a>
 	            <a
-	              href="#"
+	              href="/"
 	              className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
 	            >
 	              2
 	            </a>
 	            <a
-	              href="#"
+	              href="/"
 	              className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
 	            >
 	              3
@@ -71,25 +71,25 @@ export default function Policies() {
 	              ...
 	            </span>
 	            <a
-	              href="#"
+	              href="/"
 	              className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
 	            >
 	              8
 	            </a>
 	            <a
-	              href="#"
+	              href="/"
 	              className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
 	            >
 	              9
 	            </a>
 	            <a
-	              href="#"
+	              href="/"
 	              className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
 	            >
 	              10
 	            </a>
 	            <a
-	              href="#"
+	              href="/"
 	              className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
 	            >
 	              <span className="sr-only">Next</span>
@@ -130,20 +130,20 @@ export default function Policies() {
 		            </tr>
 		          </thead>
 		          <tbody className="bg-white divide-y divide-gray-200">
-		            {people.map((person) => (
-		              <tr key={person.email}>
+		            {policy.map((policy) => (
+		              <tr key={policy.email}>
 		                <td className="px-6 py-4 whitespace-nowrap">
 		                  <div className="flex items-center">
 		                    <div className="flex-shrink-0 h-10 w-10">
-		                      <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+		                      <img className="h-10 w-10 rounded-full" src={policy.image} alt="" />
 		                    </div>
 		                    <div className="ml-4">
-		                      <div className="text-sm font-medium text-gray-900">{person.name}</div>
+		                      <div className="text-sm font-medium text-gray-900">{policy.name}</div>
 		                    </div>
 		                  </div>
 		                </td>
 		                <td className="px-6 py-4 whitespace-nowrap">
-		                  <div className="text-sm text-gray-900">{person.description}</div>
+		                  <div className="text-sm text-gray-900">{policy.description}</div>
 		                </td>
 		                <td className="px-6 py-4 whitespace-nowrap">
 		                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
@@ -151,7 +151,7 @@ export default function Policies() {
 		                  </span>
 		                </td>
 		                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-		                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
+		                  <a href="/" className="text-indigo-600 hover:text-indigo-900">
 		                    Read More
 		                  </a>
 		                </td>
