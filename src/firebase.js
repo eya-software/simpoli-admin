0c1806655f25f4f73a0bb2cef0ef7d8c2ea3a18b
Nathan Gong <nathan.gong22@bcp.org>
2021-06-03 14:55:38 -0700

    Add functionality to create page

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
-const app  = firebase.initializeApp({
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -11,5 +12,6 @@ const app  = firebase.initializeApp({
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 })
 
-export const auth = app.auth()  
+export const auth = app.auth()
+export const firestore = app.firestore()
 export default app
