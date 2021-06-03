07f077ee827b57fed961c2d43def1ef5ea605262
Nathan Gong <nathan.gong22@bcp.org>
2021-06-03 14:07:58 -0700

    Fix bugs

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -11,13 +11,13 @@ export default function ButtonGroup({
   const middleButtonClasses = "-ml-px";
   const rightButtonClasses = "-ml-px rounded-r-md";
   const baseClasses =
-    "relative inline-flex items-center px-4 py-2 border text-sm leading-5 font-medium focus:z-10 focus:outline-none focus:border-indigo-300 dark:focus:border-indigo-600 focus:shadow-outline-indigo transition ease-in-out duration-150";
+    "inline-flex items-center px-4 py-2 border text-sm leading-5 font-medium focus:outline-none focus:border-indigo-300 dark:focus:border-indigo-600 focus:shadow-outline-indigo transition ease-in-out duration-150";
   const activeClasses = "border-indigo-600 bg-indigo-600 text-white";
   const inactiveClasses =
     "border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-100 active:bg-gray-100 active:text-gray-700";
 
   return (
-    <span className="relative z-0 inline-flex shadow-sm rounded-md">
+    <span className="inline-flex shadow-sm rounded-md">
       {options.map((option, index) => (
         <button
           type="button"
