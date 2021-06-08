03af6a6aa34dc12d3872ae49ed0ee57bd15c0160
josephp1005 <40573986+josephp1005@users.noreply.github.com>
2021-06-08 00:04:23 -0700

    Added news

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,10 +1,263 @@
-import React from "react";
+import React, { Component } from "react";
 import Page from "./Page";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import { ArchiveIcon, TrashIcon } from "@heroicons/react/outline";
+import ClipLoader from "react-spinners/ClipLoader";
+import { firestore } from "../firebase";
+import "firebase/firestore";
 
-export default function News() {
-  return (
-    <>
-      <Page name="News">Hello World</Page>
-    </>
-  );
+export default class News extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      stories: [],
+    };
+  }
+
+  getStories() {
+    this.setState({
+      loading: true,
+    });
+    firestore.collection("news").onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        let story = doc.data();
+        story["id"] = doc.id;
+        items.push(story);
+        console.log(story);
+      });
+      this.setState({
+        loading: false,
+        stories: items,
+      });
+    });
+  }
+
+  deleteStory(id) {
+    firestore
+      .collection("news")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("Delete successful");
+      });
+  }
+
+  setStoryStatus(id, status) {
+    firestore.collection("news").doc(id).update({
+      status: status,
+    });
+  }
+
+  componentDidMount() {
+    this.getStories();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <Page name="News">
+          <ClipLoader color="#4338CA" loading={true}></ClipLoader>
+        </Page>
+      );
+    }
+
+    return (
+      <>
+        <Page name="News">
+          <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+            <div className="flex-1 flex justify-between sm:hidden">
+              <a
+                href="/"
+                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+              >
+                Previous
+              </a>
+              <a
+                href="/"
+                className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+              >
+                Next
+              </a>
+            </div>
+            <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+              <div>
+                <p className="text-sm text-gray-700">
+                  Showing <span className="font-medium">1</span> to{" "}
+                  <span className="font-medium">10</span> of{" "}
+                  <span className="font-medium">97</span> results
+                </p>
+              </div>
+              <div>
+                <nav
+                  className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+                  aria-label="Pagination"
+                >
+                  <a
+                    href="/"
+                    className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                  >
+                    <span className="sr-only">Previous</span>
+                    <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+                  </a>
+                  <a
+                    href="/"
+                    aria-current="page"
+                    className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                  >
+                    1
+                  </a>
+                  <a
+                    href="/"
+                    className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                  >
+                    2
+                  </a>
+                  <a
+                    href="/"
+                    className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+                  >
+                    3
+                  </a>
+                  <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
+                    ...
+                  </span>
+                  <a
+                    href="/"
+                    className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+                  >
+                    8
+                  </a>
+                  <a
+                    href="/"
+                    className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                  >
+                    9
+                  </a>
+                  <a
+                    href="/"
+                    className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                  >
+                    10
+                  </a>
+                  <a
+                    href="/"
+                    className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                  >
+                    <span className="sr-only">Next</span>
+                    <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                  </a>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-100">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Article Title
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Article Description
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Status
+                        </th>
+                        <th scope="col" className="relative px-6 py-3">
+                          <span className="sr-only">Edit</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                      {this.state.stories.map((story) => (
+                        <tr>
+                          <td className="px-6 py-8 whitespace-nowrap">
+                            <div className="flex items-center">
+                              <div className="flex-shrink-0 h-10 w-10">
+                                <img
+                                  className="h-10 w-10 rounded-full"
+                                  src={story.image}
+                                  alt=""
+                                />
+                              </div>
+                              <div className="ml-4">
+                                <div className="text-sm font-medium text-gray-900">
+                                  {story.title}
+                                </div>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm text-gray-900">
+                              {story.description.substr(0,40)}...
+                            </div>
+                          </td>
+                          {story.status === 0 && (
+                            <td className="px-5 py-4 whitespace-nowrap">
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                Archived
+                              </span>
+                            </td>
+                          )}
+                          {story.status === 1 && (
+                            <td className="px-5 py-4 whitespace-nowrap">
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                Active
+                              </span>
+                            </td>
+                          )}
+                          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <button
+                              className="focus:outline-none"
+                              onClick={() => {
+                                this.setStoryStatus(
+                                  story.id,
+                                  1 - story.status
+                                );
+                              }}
+                            >
+                              <ArchiveIcon
+                                className="h-6 w-6 mr-4 text-indigo-600"
+                                aria-hidden="true"
+                              />
+                            </button>
+                            <button
+                              className="focus:outline-none"
+                              onClick={() => {
+                                this.deleteStory(story.id);
+                              }}
+                            >
+                              <TrashIcon
+                                className="h-6 w-6 text-red-600"
+                                aria-hidden="true"
+                              />
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Page>
+      </>
+    );
+  }
 }
